0ef47f3a7154c7fcf5cdd2cb6c4bdd045f02d92d
Samson Eitan Lewin <samsoneitanlewin@MacBook-Pro-de-Samson.local>
2017-05-12 14:58:00 -0700

    error

--- models/burger.js
+++ models/burger.js
@@ -3,9 +3,6 @@ module.exports = function(sequelize, DataTypes) {
   var Burger = sequelize.define("Burger", {
     burger_name: {
     	type: DataTypes.STRING,
-    	valdiate:{
-    	notNull: true
-    }
     },
     devoured: {
     	type: DataTypes.BOOLEAN,
